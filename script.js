88fe650a15e5a0e376563ad00770521d78c90c69
Nikita O'Keeffe <snow@Nikitas-MacBook-Air.local>
2023-06-10 11:32:57 +1000

    swapped out some Javscript styling in favour of CSS class toggles.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// import valid word array
+// IMPORT VALID WORD ARRAY
 import validWords from "./wordlist.js"
 
-// get DOM elements
+// RETRIEVE DOM ELEMENTS AND STORE THEM AS VARIABLES
 const resultMsg = document.getElementById("result-msg")
 const keyboardKey = document.querySelectorAll(".key")
 const enterKey = document.getElementById("enter")
@@ -20,36 +20,73 @@ const buttons = document.querySelectorAll(".options")
 const hintsMsg = document.getElementById("hints")
 const hintsBtn = document.getElementById("hints-btn")
 
-// initialize counters
+// INITIALIZE BUTTON & KEY EVENT LISTENERS
+function initializeEventListeners() {
+    for (let key of keyboardKey) {
+        key.addEventListener("click", keyPress);
+        key.addEventListener("click", playKeySound);
+    }
+    backspaceKey.addEventListener("click", backspace);
+    backspaceImg.addEventListener("click", backspace);
+    enterKey.addEventListener("click", enter);
+    treasureBtn.addEventListener("click", generateTreasure);
+    treasureResetBtn.addEventListener("click", generateTreasure);
+    wordleBtn.addEventListener("click", wordleMode);
+    hintsBtn.addEventListener("click", showHint);
+}
+window.onload = initializeEventListeners;
+
+// INITIALIZE GLOBAL COUNTERS
 let rowNum = 1;
 let i = 0;
+let hints = 0;
+let attempts = 0;
+let y = 0;
+let maxHints = 3;
+let lettersGiven = [];
+let hintsUsed = 0;
 
-// generate random word from valid word array
-function randomWord() {
-    let randomIndex = Math.floor(Math.random() * validWords.length);
-    return validWords[randomIndex];
+// FUNCTIONS TO RETRIEVE A RANDOM INDEX FROM AN ARRAY & TO PICK A RANDOM SQUARE INDEX
+function randomIndex(arr) {
+    return Math.floor(Math.random() * arr.length);
 }
-let secretWord = randomWord();
+
+function randomSquare() {
+    let randomIndex = Math.floor(Math.random() * squares.length);
+    return squares[randomIndex];
+}
+
+// FUNCTION TO GENERATE RANDOM "TREASURE" SQUARES FOR TREASURE HUNT
+function generateRandomTreasure() {
+    for (let square of squares) {
+        square.classList.remove("treasure");
+    }
+    
+    let randomSquare1 = randomSquare();
+    randomSquare1.classList.add("treasure")
+    console.log(randomSquare1);
+    let randomSquare2 = randomSquare();
+    randomSquare2.classList.add("treasure")
+    console.log(randomSquare2);
+    let randomSquare3 = randomSquare();
+    randomSquare3.classList.add("treasure");
+    console.log(randomSquare3);
+}
+
+// FUNCTION TO GENERATE A RANDOM SECRET WORD FROM VALID WORDS LIST
+function generateSecretWord() {
+    let randomWord = randomIndex(validWords);
+    return validWords[randomWord];
+}
+
+let secretWord = generateSecretWord();
 let lettersOfWord = secretWord.split("")
 console.log(secretWord);
 
-// add event listeners to keyboard keys
-for (let key of keyboardKey) {
-    key.addEventListener("click", keyPress);
-    key.addEventListener("click", playKeySound);
-}
-backspaceKey.addEventListener("click", backspace);
-backspaceImg.addEventListener("click", backspace);
-enterKey.addEventListener("click", enter);
-treasureBtn.addEventListener("click", generateTreasure);
-treasureResetBtn.addEventListener("click", generateTreasure);
-wordleBtn.addEventListener("click", wordleMode);
-hintsBtn.addEventListener("click", showHint);
-hintsMsg.addEventListener("click", freeHint);
-
-
-// backspace function
-function backspace() {
+// FUNCTION FOR BACKSPACE KEY TO CLEAR LETTERS, LIMITED TO THE CURRENT ROW
+function backspace(event) {
+    event.stopPropagation();
+    
     if (i > 0 && rowNum === 1) {
         squares[i -1].textContent = "";
         i--;
@@ -74,6 +111,7 @@ function backspace() {
         squares[i + 25 - 1].textContent = "";
         i--;
     }
+    
     if (i < 5) { 
         enterKey.style.backgroundColor = "#EFF9FF";
         for (let key of keyboardKey) {
@@ -95,8 +133,8 @@ function backspace() {
     }
 }
 
-// retrieve user guesses
-function getWord(row) {
+// FUNCTION TO RETRIEVE USER WORD GUESS FOR EACH ROW
+function getUserSelection(row) {
     let chosenWord = "";
     if (row === 1) {
        for (let x = 0; x < 5; x++) {
@@ -126,19 +164,24 @@ function getWord(row) {
     return chosenWord;
 }
 
-// compares user guess with secret word, changes squares color if letter is included/in the right spot
+/* FUNCTION TO GET USER WORD SELECTION FROM INPUTTED LETTERS, COMPARES USER SELECTION AGAINST VALID WORDS  LIST 
+AND SECRET WORD */
 function enter() {
     if (i === 5) {
-        let chosenWord = getWord(rowNum);
+        let chosenWord = getUserSelection(rowNum);
+        
         if (!validWords.includes(chosenWord)) {
             resultMsg.textContent = "Please enter a valid word!";
             return;
         }
+        
         let indexStart = (rowNum - 1) * 5;
         let indexEnd = rowNum * 5;
         for (let i = indexStart; i < indexEnd; i++) {
+            
             let square = squares[i];
             let guessedLetter = square.textContent;
+            
             if (lettersOfWord.includes(guessedLetter)) {
                 square.style.transition = "all 1s";
                 square.style.backgroundColor = "#F5C4A8";
@@ -153,11 +196,13 @@ function enter() {
                 square.style.opacity = "50%";
             }
         }
+        
         for (let key of keyboardKey) {
             key.style.backgroundColor = "#EFF9FF"
             key.style.color = "#0068AB"
             key.style.opacity = "100%";
         }
+        
         rowNum++
         i = 0;
         resultMsg.textContent = chosenWord;
@@ -183,7 +228,7 @@ function enter() {
     }
 }
 
-// onclick function for the keyboard keys
+// FUNCTION TO HANDLE KEYBOARD KEY PRESSES (USER LETTER SELECTION)
 function keyPress(event) {
    if (event.target.id !== "enter" && event.target.id !== "backspace" && i !== 5) {
         if (rowNum === 1) {
@@ -201,6 +246,7 @@ function keyPress(event) {
         }
         i++;
     }
+   
     if (i === 5) {
         enterKey.style.backgroundColor = "#D9F5A8"
         for (let key of keyboardKey) {
@@ -213,15 +259,25 @@ function keyPress(event) {
     }
 }
 
-function reset() {
-    lettersGiven = [];
-    secretWord = randomWord();
+// FUNCTION TO GENERATE A NEW WORD TO REPLACE PREVIOUS WORD
+function newWord() {
+    secretWord = generateSecretWord();
     console.log(secretWord);
     lettersOfWord = secretWord.split("");
+}
+
+// FUNCTION TO RESET GLOBAL COUNTERS
+function resetCounters() {
+    lettersGiven = [];
     rowNum = 1;
     i = 0;
     hintsUsed = 0;
-    resultMsg.textContent = "Guess the word!";
+    y = 0;
+    attempts = 0;
+}
+
+// FUNCTION THAT RESETS KEYBOARD KEYS TO DEFAULT STYLING
+function resetKeysDefault() {
     for (let square of squares) {
         square.textContent = "";
         square.style.backgroundColor = "#EFF9FF";
@@ -234,23 +290,26 @@ function reset() {
     }
 }
 
-function randomSquare() {
-    let randomIndex = Math.floor(Math.random() * squares.length);
-    return squares[randomIndex];
+// FUNCTION TO RESET COUNTERS, GENERATE A NEW WORD & RESET KEYBOARD KEYS TO DEFAULT STYLING ALL AT ONCE
+function reset() {
+    resetCounters();
+    newWord();
+    resetKeysDefault();
+    resultMsg.textContent = "Guess the word!";
 }
+
+// FUNCTION TO CHANGE PAGE TO TREASURE HUNT GAME-MODE WHERE USERS CAN PLAY FOR WORDLE HINTS
 function generateTreasure() {
     reset();
     if (hints > 0) {
         hintsMsg.textContent = `Hints remaining: ${hints}`;
         hintsMsg.style.display = "block";
     }
-    wordleBtn.style.display = "inline";
-    hintsBtn.style.display = "none";
-    attempts = 0;
-    y = 0;
+    wordleBtn.classList.toggle("hidden");
+    hintsBtn.classList.toggle("hidden");
+    treasureBtn.classList.toggle("hidden");
+    treasureResetBtn.classList.toggle("hidden");
     resultMsg.textContent = "Find the treasure! You have 6 guesses.";
-    treasureBtn.style.display = "none";
-    treasureResetBtn.style.display = "inline";
     for (let square of squares) {
         square.removeEventListener("click", userGuess);
         square.removeEventListener("click", playSquareSound);
@@ -260,35 +319,11 @@ function generateTreasure() {
     for (let key of keyboardKey) {
         key.removeEventListener("click", keyPress);
         key.removeEventListener("click", playKeySound);
-        key.addEventListener("mouseenter", function() {
-            key.style.backgroundColor = "#AADAF5";
-            key.style.opacity = "50%";
-            key.style.borderColor = "white";
-            key.style.transition = "all 0.15s";
-            key.style.scale = "102%";
-        })
-        key.addEventListener("mouseleave", function() {
-            key.style.backgroundColor = "#EFF9FF";
-            key.style.opacity = "100%";
-            key.style.border = "3px solid #DEF1FF";
-            key.style.scale = "100%"
-
-        })
     }
-    let randomSquare1 = randomSquare();
-    randomSquare1.classList.add("treasure")
-    console.log(randomSquare1);
-    let randomSquare2 = randomSquare();
-    randomSquare2.classList.add("treasure")
-    console.log(randomSquare2);
-    let randomSquare3 = randomSquare();
-    randomSquare3.classList.add("treasure");
-    console.log(randomSquare3);
+    generateRandomTreasure();
 }
 
-let hints = 0;
-let attempts = 0;
-let y = 0;
+// FUNCTION TO HANDLE USER GUESSES FOR TREASURE HUNT
 function userGuess(event) {
     let guessesLeft = 5 - attempts;
 
@@ -306,6 +341,7 @@ function userGuess(event) {
         }
     }
     attempts++
+    
     if (y === 3) {
         attempts = 6;
         setTimeout(function() {
@@ -313,6 +349,7 @@ function userGuess(event) {
         }, 700)
         playWinSound();
         hints++
+        
         for (let square of squares) {
             if (square.classList.contains("treasure")) {
                 square.classList.remove("treasure");
@@ -321,11 +358,13 @@ function userGuess(event) {
         generateTreasure();
         return;
     }
+    
     if (attempts > 6) {
         resultMsg.textContent = `You found ${y}/3 treasure chests!`
         if (attempts === 7) {
             playLoseSound();
         }
+        
         for (let square of squares) {
             square.removeEventListener("click", playSquareSound);
             if (square.classList.contains("treasure")) {
@@ -335,17 +374,19 @@ function userGuess(event) {
     }
 }
 
-
+// FUNCTION TO RESET PAGE BACK TO WORDLE MODE
 function wordleMode() {
-    wordleBtn.style.display = "none";
-    hintsBtn.style.display = "inline";
-    treasureResetBtn.style.display = "none";
-    treasureBtn.style.display = "inline";
+    wordleBtn.classList.toggle("hidden");
+    hintsBtn.classList.toggle("hidden");
+    treasureBtn.classList.toggle("hidden");
+    treasureResetBtn.classList.toggle("hidden");
     reset();
+    
     for (let square of squares) {
         square.removeEventListener("click", userGuess);
         square.removeEventListener("click", playSquareSound);
     }
+    
     for (let key of keyboardKey) {
         key.addEventListener("click", keyPress);
         key.addEventListener("click", playKeySound);
@@ -359,13 +400,12 @@ function wordleMode() {
     }
 }
 
+// FUNCTIONS TO GENERATE A RANDOM LETTER FROM SECRET WORD AND DISPLAY IT TO USER WHEN A HINT IS USED
 function randomHint() {
     return Math.floor(Math.random() * lettersOfWord.length);
 }
+
 let latestHint = ""
-let lettersGiven = [];
-let hintsUsed = 0;
-let maxHints = 3;
 function showHint() {
     let newHint = ""
     if (hints > 0 && hintsUsed < maxHints) {
@@ -391,11 +431,8 @@ function showHint() {
     }
 }
 
-function freeHint() {
-    hints++
-    hintsMsg.textContent = `Hints remaining: ${hints}`;
-}
 
+// AUDIO FUNCTIONS FOR KEYS & BUTTONS
 function playSound() {
     btnAudio.currentTime = 0;
     btnAudio.play();
@@ -403,22 +440,18 @@ function playSound() {
 for (let button of buttons) {
     button.addEventListener("click", playSound)
 }
-
 function playWinSound() {
     winAudio.currentTime = 0;
     winAudio.play();
 }
-
 function playLoseSound() {
     loseAudio.currentTime = 0;
     loseAudio.play();
 }
-
 function playKeySound() {
     keyAudio.currentTime = 0;
     keyAudio.play();
 }
-
 function playSquareSound() {
     squareAudio.currentTime = 0;
     squareAudio.play();
